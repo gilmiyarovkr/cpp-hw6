198485ba5d75e831af9fa7defa07159110874d86
gilmiyarovkr <gilmiyarovkr@gmail.com>
2026-08-02 22:54:21 +0300

    Добавлены тесты

--- app/main.cpp
+++ app/main.cpp
@@ -1,41 +1,8 @@
 #include <iostream>
-#include <cassert>
 #include "matrix.hpp"
 
 int main()
 {
-    // бесконечная матрица int заполнена значениями -1
-    // Matrix <int, 2, -1> matrix;
-
-    // assert(matrix.size() == 0);
-    // std::cout << (matrix.size() == 0) << std::endl;
-
-    // // все ячейки свободны
-    // auto a = matrix[0][0];
-    // assert(a == -1);
-    // std::cout << (a == -1) << std::endl;
-
-    // assert(matrix.size() == 0);
-    // std::cout << (matrix.size() == 0) << std::endl;
-
-    // matrix[100][100] = 314;
-    // assert(matrix[100][100] == 314);
-    // std::cout << (matrix[100][100] == 314) << std::endl;
-
-    // assert(matrix.size() == 1);
-    // std::cout << (matrix.size() == 1) << std::endl;
-
-    // // выведется одна строка
-    // // 100100314
-    // for(auto c : matrix)
-    // {
-    //     int x;
-    //     int y;
-    //     int v;
-    //     std::tie(x, y, v) = c;
-    //     std::cout << x << y << v << std::endl;
-    // }
-
     Matrix <int, 2, 0> matrix;
 
     // Заполнение матрицы
---
+++ tests/gtest.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <tuple>
+#include "matrix.hpp"
+
+TEST(MatrixTest, BasicOperations2D) {
+    Matrix<int, 2, -1> matrix;
+
+    EXPECT_EQ(matrix.size(), 0U);
+
+    auto a = matrix[0][0];
+    EXPECT_EQ(a, -1);
+    EXPECT_EQ(matrix.size(), 0U);
+
+    matrix[100][100] = 314;
+    EXPECT_EQ(matrix[100][100], 314);
+    EXPECT_EQ(matrix.size(), 1U);
+
+    bool element_found = false;
+    for (auto c : matrix) {
+        int x, y, v;
+        std::tie(x, y, v) = c;
+
+        EXPECT_EQ(x, 100);
+        EXPECT_EQ(y, 100);
+        EXPECT_EQ(v, 314);
+        element_found = true;
+    }
+    EXPECT_TRUE(element_found);
+}
+
+TEST(MatrixTest, MultiDimensionalOperations5D) {
+    Matrix<int, 5, 0> matrix5D;
+
+    EXPECT_EQ(matrix5D.size(), 0U);
+
+    int default_val = matrix5D[1][2][3][4][5];
+    EXPECT_EQ(default_val, 0);
+    EXPECT_EQ(matrix5D.size(), 0U);
+
+    matrix5D[10][20][30][40][50] = 42;
+    EXPECT_EQ(matrix5D[10][20][30][40][50], 42);
+    EXPECT_EQ(matrix5D.size(), 1U);
+
+    bool element_found = false;
+    for (auto cell : matrix5D) {
+        auto [i1, i2, i3, i4, i5, val] = cell;
+
+        EXPECT_EQ(i1, 10);
+        EXPECT_EQ(i2, 20);
+        EXPECT_EQ(i3, 30);
+        EXPECT_EQ(i4, 40);
+        EXPECT_EQ(i5, 50);
+        EXPECT_EQ(val, 42);
+        element_found = true;
+    }
+    EXPECT_TRUE(element_found);
+
+    matrix5D[10][20][30][40][50] = 0;
+    EXPECT_EQ(matrix5D.size(), 0U); // Память успешно освобождена
+    EXPECT_EQ(matrix5D[10][20][30][40][50], 0); // Возвращается дефолт
+}
